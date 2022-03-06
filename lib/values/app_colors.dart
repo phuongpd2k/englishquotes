6e7fa7e677971233d880e888fbe60df4d462e0cd
zuyfun <zuyfun@gmail.com>
2022-03-06 15:55:56 +0700

    init project

---
+++ lib/values/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xffABC4FF);
+  static const Color secondaryColor = Color(0xffE5E5E5);
+  static const Color blackGray = Color(0xff777777);
+}
